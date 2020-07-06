166c284466196d2136dd77b5df8433ab17177a7c
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-06 12:21:21 +0300

    Add casche for offline data

--- Core/Services/OfflineDataAccessService.cs
+++ Core/Services/OfflineDataAccessService.cs
@@ -8,34 +8,44 @@ namespace Core.Services
 {
     public class OfflineDataAccessService : IDataAccessService
     {
-        public IQueryable<Appointment> GetAppointments()
-        {
-            IQueryable<Patient> patients = GetPatients();
-            IQueryable<Doctor> doctors = GetDoctors();
+        IQueryable<Patient> Patients;
+        IQueryable<Doctor> Doctors;
+        IQueryable<Appointment> Appointments;
 
-
-            return (new List<Appointment>
-            {
-                new Appointment {Patient = patients.ElementAt(0), Doctor = doctors.ElementAt(0), Time = DateTime.Now.AddDays(1)},
-                new Appointment {Patient = patients.ElementAt(1), Doctor = doctors.ElementAt(1), Time = DateTime.Now.AddDays(3)}
-            }).AsQueryable();
-        }
-        public IQueryable<Doctor> GetDoctors()
+        public OfflineDataAccessService()
         {
-            return (new List<Doctor>
-            {
-                new Doctor { Id = Guid.NewGuid().ToString(), Name = "sadad as asd"},
-                new Doctor { Id = Guid.NewGuid().ToString(), Name = "s sda aasdq"}
-            }).AsQueryable();
-        }
-
-        public IQueryable<Patient> GetPatients()
-        {
-            return (new List<Patient>
+            Patients = (new List<Patient>
             {
                 new Patient { Id = Guid.NewGuid().ToString(), Name = "dsad geer", Height = 170, Weight = 67.6f, PassportInfo = "rehtrg", InsuranceInfo = "5435"},
                 new Patient { Id = Guid.NewGuid().ToString(), Name = "dhdf sdsfs", Height = 150, Weight = 47.6f, PassportInfo = "rg", InsuranceInfo = "5ytrydf5"},
             }).AsQueryable();
+
+            Doctors = (new List<Doctor>
+            {
+                new Doctor { Id = Guid.NewGuid().ToString(), Name = "sadad as asd"},
+                new Doctor { Id = Guid.NewGuid().ToString(), Name = "s sda aasdq"}
+            }).AsQueryable();
+
+            Appointments = (new List<Appointment>
+            {
+                new Appointment {Patient = Patients.ElementAt(0), Doctor = Doctors.ElementAt(0), Time = DateTime.Now.AddDays(1)},
+                new Appointment {Patient = Patients.ElementAt(0), Doctor = Doctors.ElementAt(1), Time = DateTime.Now.AddDays(2)},
+                new Appointment {Patient = Patients.ElementAt(1), Doctor = Doctors.ElementAt(1), Time = DateTime.Now.AddDays(3)}
+            }).AsQueryable();
+        }
+
+        public IQueryable<Appointment> GetAppointments()
+        {
+            return Appointments;
+        }
+        public IQueryable<Doctor> GetDoctors()
+        {
+            return Doctors;
+        }
+
+        public IQueryable<Patient> GetPatients()
+        {
+            return Patients;
 
         }
     }
