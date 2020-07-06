c157bfcd2e511eac2634b08e88a336b446f4416d
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-06 09:30:35 +0300

    Doctor service

---
+++ Core/Services/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    interface IEmployeeService
+    {
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        protected IDataAccessService DataAccessService;
+
+        public EmployeeService(IDataAccessService dataAccessService)
+        {
+            DataAccessService = dataAccessService;
+        }
+    }
+}
