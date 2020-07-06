35b68727954f2fd1fc49d98ed9f83a7b788d22a1
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-06 12:22:42 +0300

    Add test for busyness sorting

--- Tests/CoreServicesTest.cs
+++ Tests/CoreServicesTest.cs
@@ -12,16 +12,27 @@ namespace Tests
     public class CoreServicesTest
     {
         [TestMethod]
-        public void TestAppointmentCreation()
+        public void AppointmentCreationTest()
         {
             IDataAccessService data = new OfflineDataAccessService();
             IAppointmentService appointmentService = new AppointmentService(data);
-            
+
             Doctor doc = data.GetDoctors().ElementAt(1);
             IEnumerable<Appointment> list = appointmentService.GetAppointments(doc);
 
             foreach (var app in list)
-                Assert.AreEqual(app.Doctor, doc);
+                Assert.AreEqual(app.Doctor.GetHashCode(), doc.GetHashCode());
+        }
+
+        [TestMethod]
+        public void DoctorSortByBusynessTest()
+        {
+            IDataAccessService data = new OfflineDataAccessService();
+            DoctorService ds = new DoctorService(data);
+
+            Doctor doctor = ds.SortDoctorsByBusyness().ElementAt(0);
+            Doctor expected = data.GetDoctors().ElementAt(0);
+            Assert.AreEqual(expected.GetHashCode(), doctor.GetHashCode());
         }
     }
 }
