7697f9d04901ed644c0f541f1ffe436b3de7d125
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-08 10:08:30 +0300

    Add AppointmentService to Site

--- App/Controllers/AppointmentController.cs
+++ App/Controllers/AppointmentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core;
 using Core.Models;
+using Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,16 +15,31 @@ namespace App.Controllers
     public class AppointmentController : ControllerBase
     {
         private readonly IDataAccessService DataAccessService;
+        private readonly IAppointmentService AppointmentService;
 
-        public AppointmentController(IDataAccessService dataAccessService)
+        public AppointmentController(IDataAccessService dataAccessService, IAppointmentService appointmentService)
         {
             DataAccessService = dataAccessService;
+            AppointmentService = appointmentService;
         }
 
         [HttpGet]
         public IEnumerable<Appointment> Get()
         {
             return DataAccessService.GetAppointments().AsEnumerable();
+        }
+        [HttpGet("patient")]
+        public IEnumerable<Appointment> GetPatientAppts(string id)
+        {
+            var patient = DataAccessService.GetPatients().FirstOrDefault(p => p.Id == id);
+            var appts = AppointmentService.GetAppointments(patient);
+            return appts;
+        }
+        [HttpGet("doctor")]
+        public IEnumerable<Appointment> GetDoctorAppts(string id)
+        {
+            var doctor = DataAccessService.GetDoctors().FirstOrDefault(d => d.Id == id);
+            return AppointmentService.GetAppointments(doctor);
         }
     }
 }
--- App/Startup.cs
+++ App/Startup.cs
@@ -9,6 +9,7 @@ using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using React.AspNet;
@@ -17,9 +18,19 @@ namespace App
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IDataAccessService, OfflineDataAccessService>();
+            services.AddSingleton<IAppointmentService, AppointmentService>(serviceProvider =>
+            {
+                return new AppointmentService(serviceProvider.GetService<IDataAccessService>());
+            });
             services.AddControllers();
 
             services.AddMemoryCache();
