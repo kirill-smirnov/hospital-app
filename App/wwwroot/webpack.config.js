7c314f73a330a38a849dd7b49078fa7964fed1ce
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-10 11:30:32 +0300

    Add css and file loaders for webpack

--- App/wwwroot/webpack.config.js
+++ App/wwwroot/webpack.config.js
@@ -16,6 +16,22 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   }
 }
