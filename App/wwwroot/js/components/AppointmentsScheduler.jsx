b88b2abc6d58f4a7dfa8a14a3e3101f044aaa449
Smirnov Kirill <smirnov.k@yahoo.com>
2020-07-13 10:07:42 +0300

    Add scheduler for specific doctor

--- App/wwwroot/js/components/AppointmentsScheduler.jsx
+++ App/wwwroot/js/components/AppointmentsScheduler.jsx
@@ -4,31 +4,60 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import Scheduler from 'devextreme-react/Scheduler';
 
-import { appointmentsFetchAsync } from '../services/dataFetch';
+import DoctorSelect from './DoctorSelect.jsx';
+import { appointmentsFetchAsync, doctorsFetchAsync } from '../services/dataFetch';
 
 class AppointmentsScheduler extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      doctors: [],
+      selectedDoctor: ''
+    };
+  }
+
+  onSelectChange(e) {
+    const doctorId = e.target.value;
+
+    appointmentsFetchAsync(doctorId)
+      .then((data) => {
+        this.setState({ appointments: data });
+      })
+      .then(() => this.setState({ selectedDoctor: doctorId }));
   }
 
   async componentDidMount() {
     await appointmentsFetchAsync()
       .then((data) => {
         this.setState({ appointments: data });
-      })
+      });
+
+    await doctorsFetchAsync()
+    .then(doctors => {
+      this.setState({
+        doctors
+      });
+    });
   }
 
   render() {
     return (
-      <Scheduler
-        dataSource={this.state.appointments}
-        views= {['day', 'workWeek', 'month']}
-        defaultCurrentView="month"
-        textExpr="patient.name"
-        startDateExpr="start"
-        endDateExpr="end" />
-  );
+      <div>
+        <DoctorSelect 
+          value={this.state.selectedDoctor}
+          data={this.state.doctors}
+          onChange={this.onSelectChange.bind(this)} />
+        <Scheduler
+          dataSource={this.state.appointments}
+          views={['day', 'workWeek', 'month']}
+          defaultCurrentView="month"
+          height={300}
+          width={700}
+          textExpr="patient.name"
+          startDateExpr="start"
+          endDateExpr="end" />
+      </div>
+    );
   }
 }
 
--- App/wwwroot/js/services/dataFetch.js
+++ App/wwwroot/js/services/dataFetch.js
@@ -1,8 +1,17 @@
-async function appointmentsFetchAsync() {
-  return await fetch('api/appointments')
+async function appointmentsFetchAsync(doctorId) {
+  const url = doctorId ?
+    `api/appointments?doctorId=${doctorId}`:
+    'api/appointments'
+
+  return await fetch(url)
       .then(res => res.json())
       .catch(console.log);
 }
 
+async function doctorsFetchAsync() {
+  return await fetch('api/doctors')
+      .then(res => res.json())
+      .catch(console.log);
+}
 
-export { appointmentsFetchAsync }
+export { appointmentsFetchAsync, doctorsFetchAsync }
